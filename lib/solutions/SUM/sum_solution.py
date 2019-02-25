9d2dac5fabb45e6765d395d07ceca4571ac6de52
sbx_user1060 <sbx_user1060@ip-10-128-138-69.eu-west-2.compute.internal>
2019-02-25 18:09:41 +0000

    Mon Feb 25 18:09:41 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,16 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def sumR1(Int1,Int2):
+    '''
+
+    :param Int1: Integer between 0 and 100 inclusive
+    :param Int2: Integer between 0 and 100 inclusive
+    :return: Sum of Int1 and Int2
+
+    >>> sumR1 (3, 57) --> 60
+    >>> sumR1 (0, 100) --> 60
+    '''
+
+    if Int1 in range(0.101) and Int2 in range(0,101):
+        print Int1+Int2
+    else:
+        print 'Input out of range'
