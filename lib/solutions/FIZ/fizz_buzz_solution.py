5f6a2083f0439638a86b6a0a072b30ab7b692ba3
sbx_user1060 <sbx_user1060@ip-10-128-138-69.eu-west-2.compute.internal>
2019-02-25 18:55:28 +0000

    Mon Feb 25 18:55:28 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,3 +1,17 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
+
+    '''
+
+    :param number: Integer
+    :param Int2: Integer between 0 and 100 inclusive
+    :return: string
+
+    >>> fizz_buzz (3) --> 60
+    >>> sumR1 (0, 100) --> 60
+    '''
+
+
+
     raise NotImplementedError()
+
