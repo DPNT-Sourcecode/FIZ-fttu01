9109d28b4004f363b2b2718d8658290bb84c4b86
sbx_user1060 <sbx_user1060@ip-10-128-138-69.eu-west-2.compute.internal>
2019-02-25 18:43:04 +0000

    Mon Feb 25 18:43:04 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,17 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    '''
+
+     :param Int1: Integer between 0 and 100 inclusive
+     :param Int2: Integer between 0 and 100 inclusive
+     :return: Sum of Int1 and Int2
+
+     >>> hello ('Bob') --> 'Hello Bob'
+     >>> sumR1 ('Alice') --> 'Hello Alice'
+     '''
+    msg ='Hello '
+    return msg + friend_name
+
+    '''raise NotImplementedError()'''
+
