8aef28380a983409fe3d9811e0a55765a064771e
sbx_user1059 <sbx_user1059@ip-10-129-112-167.eu-west-2.compute.internal>
2019-02-27 19:52:30 +0000

    Wed Feb 27 19:52:30 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,37 +1,69 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-
     '''
-
     :param number: Integer
-    :param Int2: Integer between 0 and 100 inclusive
     :return: string
 
     >>> fizz_buzz (3) --> 'fizz'
     >>> fizz_buzz (5) --> 'buzz'
     >>> fizz_buzz (15) --> 'fizz buzz'
+    >>> fizz_buzz (333) --> 'fizz deluxe'
+    >>> fizz_buzz (555) --> 'buzz deluxe'
     '''
 
     both = 'fizz buzz'
     one = 'fizz'
     butNotTheOther = 'buzz'
-    msg =''
+    poshFrocks = ' deluxe'
+    msg = ''
 
+    ''' Test for divisible by 3 or contains 3 '''
     if not (number % 3):
         msg = one
     elif '3' in str(number):
         msg = one
 
+    ''' Test for divisible by 5 or contains 5 '''
     if not (number % 5):
         msg = msg + butNotTheOther
     elif '5' in str(number):
         msg = msg + butNotTheOther
 
-    if len(msg) > 4:
+    ''' Test for bigger than ten and number made
+    up of the same character repeated '''
+    if number > 10 and singleNum(number):
+        if len(msg) == 4:
+            msg = msg + poshFrocks
+        else:
+            msg = both + poshFrocks
+
+    if len(msg) > 10:
+        return msg
+    elif len(msg) > 4 and len(msg) < 10:
         return both
     elif len(msg) == 4:
         return msg
     else:
         return str(number)
+
+    '''raise NotImplementedError()'''
+
+
+def singleNum(number):
+    '''
+    :param number: Integer
+    :return: boolean
+
+    >>> singleNum(111) --> 1
+    >>> singleNum(112) --> 0
+    '''
+
+    ''' Get the first charcter in the number '''
+    num = str(number)[0]
+
+    '''If all the digits in number are the same
+    as the first character return TRUE (1)'''
+
+    return len(str(number).strip(num)) == 0
 
     '''raise NotImplementedError()'''
